964b0581f0c9610fd271eede9dd87f50fa6bfed1
V_ Vignesh (vigneshv7@deloitte.com) <vigneshv7@your.hostname.com>
2023-07-23 12:56:51 +0530

    Updated server.js and package.json

--- server.js
+++ server.js
@@ -3,31 +3,37 @@ import mongoose from "mongoose";
 import companyRouter from "./routes/companyRoutes.js";
 import hackathonRouter from "./routes/hackathonRoute.js";
 import userRouter from "./routes/userRoutes.js";
-import participantRouter from "./routes/participantRoutes.js"
+import participantRouter from "./routes/participantRoutes.js";
 import bodyParser from "body-parser";
-import cors from "cors"
+import cors from "cors";
+import dotenv from "dotenv"; 
 
+dotenv.config();
 
-const app= express()
+const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://username:XiHzba1lVneKkEuW@cluster0.vva96ak.mongodb.net/hackathonDB?retryWrites=true&w=majority').then(()=>{
-    console.log("Connected to db");
-}).catch((err)=>{
-    console.log(err);
-}
-)
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
 
 app.use("/company", companyRouter);
 app.use("/hackathon", hackathonRouter);
 app.use("/user", userRouter);
 app.use("/participant", participantRouter);
 
-
-export const server=app.listen(3000, async(req,res)=>{
-    console.log("Listening on port 3000");
-
-})
-
+export const server = app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + (process.env.PORT || 3000));
+});
